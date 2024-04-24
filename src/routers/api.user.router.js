0f6b1e1df6353fd948123d58b538207c37fbfe0a
Igor Shilov <borkts@gmail.com>
2024-04-24 18:18:49 +0500

    papaparse

--- src/routers/api.user.router.js
+++ src/routers/api.user.router.js
@@ -1,4 +1,17 @@
 const userRouter = require("express").Router();
 const { User } = require("../../db/models");
 
+userRouter.get("/", async (req, res) => {
+  // if (req.session.login === "admin") {
+    try {
+      const allUsers = await User.findAll({ raw: true });
+      res.json(allUsers)
+    } catch (error) {
+      console.log(error);
+    }
+  // } else {
+    // res.redirect("/");
+  // }
+});
+
 module.exports = userRouter;
