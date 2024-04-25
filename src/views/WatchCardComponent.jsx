1b8eac14979e0f55517728e00e018cf4e6b45b68
Roman_Soloviev <New.akkaynt.my@gmail.com>
2024-04-25 14:34:27 +0300

    change css

--- src/views/WatchCardComponent.jsx
+++ src/views/WatchCardComponent.jsx
@@ -3,13 +3,17 @@ const React = require("react");
 function WatchComponentCard({ watch, login }) {
   return (
     <div className="watchCard">
-      <img src={watch.img} />
+      <img className="img" src={watch.img} />
       <p>{watch.title}</p>
       <p>{watch.description}</p>
-      {login === 'admin' && (
-        <div className='admin-buttons'>
-          <button id={watch.id} className='delBtn' type='button'>Удалить</button>
-          <button id={watch.id} className='changeBtn' type='button'>Отредактировать</button>
+      {login === "admin" && (
+        <div className="admin-buttons">
+          <button id={watch.id} className="delBtn" type="button">
+            Удалить
+          </button>
+          <button id={watch.id} className="changeBtn" type="button">
+            Отредактировать
+          </button>
         </div>
       )}
     </div>
