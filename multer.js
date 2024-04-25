8171150f63e7702d907f5d3ea72592011fa4254d
ZalinaMag <zalina.mag@gmail.com>
2024-04-25 14:54:55 +0300

    file rename

--- multer.js
+++ multer.js
@@ -1,10 +1,12 @@
 const multer = require('multer');
+const data = new Date();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, './public/assets/uploads');
   },
   filename(req, file, cb) {
+    file.originalname = `${data.getHours()}${data.getMinutes()}${data.getSeconds()}${file.originalname}`;
     cb(null, file.originalname);
   },
 });
