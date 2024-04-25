3c58be2a5d517d6de258c7814eff01742b2430d9
ZalinaMag <zalina.mag@gmail.com>
2024-04-25 14:12:46 +0300

    clock done

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,16 +1,21 @@
-const hour = document.querySelector('#hour');
-const minute = document.querySelector('#hour');
-const second = document.querySelector('#hour');
-setInterval(() => {
-  const d = new Date(); // object of date()
-  const hr = d.getHours();
-  const min = d.getMinutes();
-  const sec = d.getSeconds();
-  const hrRotation = 30 * hr + min / 2; // converting current time
-  const minRotation = 6 * min;
-  const secRotation = 6 * sec;
+document.addEventListener('DOMContentLoaded', () => {
+  const hour = document.querySelector('#hour');
+  const minute = document.querySelector('#minute');
+  const second = document.querySelector('#second');
 
-  hour.style.transform = `rotate(${hrRotation}deg)`;
-  minute.style.transform = `rotate(${minRotation}deg)`;
-  second.style.transform = `rotate(${secRotation}deg)`;
-}, 1000);
+  setInterval(() => {
+    const d = new Date();
+    const hr = d.getHours();
+    const min = d.getMinutes();
+    const sec = d.getSeconds();
+    const hrRotation = 30 * hr + min / 2;
+    const minRotation = 6 * min;
+    const secRotation = 6 * sec;
+
+    if (hour && minute && second) {
+      hour.style.transform = `rotate(${hrRotation}deg)`;
+      minute.style.transform = `rotate(${minRotation}deg)`;
+      second.style.transform = `rotate(${secRotation}deg)`;
+    }
+  }, 1000);
+});
