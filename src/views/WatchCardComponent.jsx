36cceae7c8d183f82e91751aa88e5a43378a3bb7
Roman_Soloviev <New.akkaynt.my@gmail.com>
2024-04-25 10:32:44 +0300

    change1

--- src/views/WatchCardComponent.jsx
+++ src/views/WatchCardComponent.jsx
@@ -1,16 +1,20 @@
-const React = require('react');
+const React = require("react");
 
 function WatchComponentCard({ watch, login }) {
   return (
-    <div className='watchCard'>
+    <div className="watchCard">
       <img src={watch.img} />
       <p>{watch.title}</p>
       <p>{watch.description}</p>
-      {login === 'admin' && (
+      {login === "admin" && (
         <>
-          <button id={watch.id} className='delBtn' type='button'>Удалить</button>
+          <button id={watch.id} className="delBtn" type="button">
+            Удалить
+          </button>
           <a href={`/change/${watch.id}`}>
-            <button id={watch.id} className='changeBtn' type='button'>Отредактировать</button>
+            <button id={watch.id} className="changeBtn" type="button">
+              Отредактировать
+            </button>
           </a>
         </>
       )}
